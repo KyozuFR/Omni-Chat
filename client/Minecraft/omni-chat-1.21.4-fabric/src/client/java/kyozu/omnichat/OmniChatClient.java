60d2444508eb063594c89f188333702a594a95ad
jlinares <julien.linares@etudiant.univ-lr.fr>
2025-05-26 10:29:13 +0200

    Minecraft Mod Creation

---
+++ client/Minecraft/omni-chat-1.21.4-fabric/src/client/java/kyozu/omnichat/OmniChatClient.java
@@ -0,0 +1,10 @@
+package kyozu.omnichat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OmniChatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
