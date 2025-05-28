a183055b2fb1f4ce5732e1003f5a9dcba3919129
SautiereQDev <contact@quentinsautiere.com>
2025-05-28 13:51:13 +0200

    finish webclient

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-      <nav class="bg-blue-900 text-white text-lg font-bold">
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+    <nav class="bg-blue-900 text-white text-2xl font-bold py-2 px-4">
+      <p>Omni-Chat</p>
+    </nav>
   </header>
-
   <RouterView />
 </template>
